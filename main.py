d5972232a03c12883468d6716d7b6a0614fc8bb5
CulinaryDelights <64852093+CulinaryDelights@users.noreply.github.com>
2021-12-05 10:28:07 -0500

    I added tenses(not 'ed')

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import random
 
-
 def RandomList(selected_list):
     return selected_list[random.randint(0, len(selected_list) - 1)]
 
+def TenseIt(verb,ending):
+    if ending == "ing":
+        if verb[-1] == 'e':
+            newVerb = verb[:-1] + "ing"
+            return newVerb
+        else:
+            newVerb = verb + "ing"
+            return newVerb
+    elif ending == "s":
+        if verb[-1] == 'y':
+            newVerb = verb[:-1] + "ies"
+            return newVerb
+        else:
+            newVerb = verb + "s"
+            return newVerb
 
 nouns = ["pizza", "tin can", "shark", "headphone", "light", "video game", "cousin", "waffle", "monster", "ukulele",
          "salami", "pig ear", "ear wig", "skeleton", "mustache", "criminal", "meatball", "finger nail", "nose hair",
-         "banjo", "rodent", "gama ray", "suit case", "jacket", "decapitated head", "flugal horn", "painting",
+         "banjo", "rodent", "gamma ray", "suitcase", "jacket", "decapitated head", "flugal horn", "painting",
          "tea biscuit", "toilet bowl", "jelly fish", "flower", "banana", "ant farm", "lightning", "tv remote",
          "knee", "yarn", "vacuum"]
 
@@ -33,7 +47,7 @@ adjectives = ["bored", "thinking", "smelly", "tired", "lazy", "happy", "excited"
 
 sentence_beginnings = ["Once upon a time", "in the beginning",
                        f"I remember when",
-                       f"{random.randint(1, 1000000)} years ago", "when I was your age",
+                       f"{random.randint(1, 1000000)} years ago", "When I was your age",
                        f"Before {RandomList(nouns)} was invented",
                        f"Before {RandomList(adjectives)} {RandomList(nouns)}s could {RandomList(verbs)}",
                        f"Back in {RandomList(noun_places)}",
@@ -44,18 +58,18 @@ sentence_beginnings = ["Once upon a time", "in the beginning",
                        f"If you really want to {RandomList(verbs)}",
                        f"Despite what {RandomList(noun_people)} said"]
 
-sentence_ends = [f"a {RandomList(adjectives)} {RandomList(nouns)} was {RandomList(verbs)}ing.",
+sentence_ends = [f"a {RandomList(adjectives)} {RandomList(nouns)} was {TenseIt(RandomList(verbs),'ing')}.",
                  f"I {RandomList(verbs)} all the time, almost constantly!",
-                 f"my {RandomList(adjectives)} {RandomList(nouns)}s favorite thing to do was {RandomList(verbs)}ing.",
+                 f"my {RandomList(adjectives)} {RandomList(nouns)}s favorite thing to do was {TenseIt(RandomList(verbs),'ing')}.",
                  f"a {RandomList(adjectives)} {RandomList(nouns)} was {RandomList(adjectives)}.",
                  f"I met {RandomList(noun_people)}.",
-                 f"everyone in {RandomList(noun_places)} was {RandomList(verbs)}ing.",
+                 f"everyone in {RandomList(noun_places)} was {TenseIt(RandomList(verbs),'ing')}.",
                  f"{RandomList(noun_people)} was {RandomList(adjectives)}.",
                  f"{RandomList(noun_people)} was the talk of {RandomList(noun_places)}.",
                  f"my favorite place is {RandomList(noun_places)}.",
                  f"I could {RandomList(verbs)} in {RandomList(noun_places)}.",
                  f"I {RandomList(verbs)} with {RandomList(noun_people)}.",
-                 f"{RandomList(verbs)}ing reminded me of {RandomList(nouns)}.",
+                 f"{TenseIt(RandomList(verbs),'ing')} reminded me of {RandomList(nouns)}.",
                  f"{RandomList(nouns)}s reminded me of {RandomList(noun_people)}."]
 
 print(RandomList(sentence_beginnings),
